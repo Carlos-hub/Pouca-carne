d427408231a893aefa94167b0b4f1d2e41afd624
Carlos-hub <delaracarlosmiguel@gmail.com>
2022-11-24 15:26:04 -0300

    create-index

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
+import { Index } from './pages/Index';
 import { Login } from './pages/Login';
 import { Signup } from './pages/Signup';
 
@@ -7,6 +8,7 @@ export function Router(){
         <Routes>
             <Route path="/login" element={<Login/>} />
             <Route path="/signup" element={<Signup/>} />
+            <Route path="/" element={<Index/>} />
         </Routes>
     )
 }
---
+++ src/pages/Index.tsx
@@ -0,0 +1,15 @@
+
+
+export function Index(){
+ return(
+  <>
+  <h1>Hello World</h1>
+  <div>
+  <a href="/login">Login</a>
+  </div>
+  <div>
+  <a href="/signup">Signup</a>
+  </div>
+  </>
+ )
+}
