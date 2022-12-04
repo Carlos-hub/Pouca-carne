cedfa1524d2e75d8e1b1f42b30a9c7ebd892faac
Carlos-hub <delaracarlosmiguel@gmail.com>
2022-12-04 14:26:19 -0300

    adicionando requsiçoes post

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -15,37 +15,37 @@ export function Signup(){
          <div className="grid grid-cols-2 gap-2">
             <label className="mt-2">
             <p className="text-[#C9E265]">Nome</p>
-            <Input classname="flex rounded-md p-1 w-full" type="text" title="name" placeholder="Nome"/>
+            <Input classname="flex rounded-md p-1 w-full" type="text" title="name" placeholder="Nome" onChange={undefined} value={undefined}/>
             </label>
             <label className="mt-2">
             <p className="text-[#C9E265]">Nascimento</p>
-            <Input classname="flex rounded-md p-1 w-full" type="date" title="date" placeholder="XX/XX/XXXX"/>
+            <Input classname="flex rounded-md p-1 w-full" type="date" title="date" placeholder="XX/XX/XXXX" onChange={undefined} value={undefined}/>
             </label>
          </div>
          <div className="grid grid-cols-2 gap-2">
              <label className="mt-2">
              <p className="text-[#C9E265]">CPF</p>
-             <Input classname="flex rounded-md p-1 w-full" type="text" title="cpf" placeholder="XXX.XXX.XXX-XX"/>
+             <Input classname="flex rounded-md p-1 w-full" type="text" title="cpf" placeholder="XXX.XXX.XXX-XX" onChange={undefined} value={undefined}/>
              </label>
              <label className="mt-2">
              <p className="text-[#C9E265]">Telefone</p>
-             <Input classname="flex rounded-md p-1 w-full" type="tel" title="phone" placeholder="(XX) X XXXX-XXXX"/>
+             <Input classname="flex rounded-md p-1 w-full" type="tel" title="phone" placeholder="(XX) X XXXX-XXXX" onChange={undefined} value={undefined}/>
              </label>
          </div>
          <label className="mt-2">
              <p className="text-[#C9E265]">E-mail</p>
-             <Input classname="flex rounded-md p-1 w-full" type="email" title="email" placeholder="teste@gmail.com"/>
+             <Input classname="flex rounded-md p-1 w-full" type="email" title="email" placeholder="teste@gmail.com" onChange={undefined} value={undefined}/>
          </label>
          <div className="w-1/2">
          <label className="mt-2">
            <p className="text-[#C9E265]">Senha</p>
-           <Input classname="flex rounded-md p-1 w-full" type="password" title="email" placeholder="**********"/>
+           <Input classname="flex rounded-md p-1 w-full" type="password" title="email" placeholder="**********" onChange={undefined} value={undefined}/>
          </label>
          </div>
          <div className="w-1/2">
          <label className="mt-2">
            <p className="text-[#C9E265]">Repita sua senha</p>
-           <Input classname="flex rounded-md p-1 w-full" type="password" title="email" placeholder="**********"/>
+           <Input classname="flex rounded-md p-1 w-full" type="password" title="email" placeholder="**********" onChange={undefined} value={undefined}/>
          </label>
          </div>
          <div className="grid grid-cols-2 gap-2">
