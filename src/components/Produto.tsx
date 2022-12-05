2861fa571d90ac4ee98d880b76efd12b7e259bd5
Carlos-hub <delaracarlosmiguel@gmail.com>
2022-12-05 18:48:56 -0300

    ajustando erros de tipagem

--- src/components/Produto.tsx
+++ src/components/Produto.tsx
@@ -11,11 +11,11 @@ interface ICreateProdutos{
 
 export function Produto(props:ICreateProdutos){
    const Navigate = useNavigate();
-   function toShop(e: MouseEvent<Element, MouseEvent>){
+   function toShop(e: MouseEvent<SVGSVGElement, MouseEvent>){
       e.preventDefault();
       Navigate('/shop')
    }
-   function toLogin(e: MouseEvent<Element, MouseEvent>){
+   function toLogin(e: MouseEvent<SVGSVGElement, MouseEvent>){
       e.preventDefault();
       Navigate('/login')
    }
